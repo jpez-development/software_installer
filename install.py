7b7113e8c6203d50f4c5141f39471fb6fa9e321c
Joshua Perry <45966243+jpez-development@users.noreply.github.com>
2022-04-27 18:42:35 +0100

    commented code

--- install.py
+++ install.py
@@ -1,4 +1,9 @@
+#This script installs all the software specified in software_list.txt
+
+#import dependencies
 import os, sys
 
+#loop through lines in text file
 for line in open(f"{sys.argv[1]}software_list.txt", "r"):
+    #install software and print command output to cmd
     print(os.system(f"choco install -y {line}"))
--- setup.bat
+++ setup.bat
@@ -1,5 +1,15 @@
+::==============================================
+::The code below installs chocolatey and python
+::then runs the python script
+::==============================================
+
+::Install chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+::Install python
 choco install python
+::Run python script
 python %~dp0install.py %~dp0
+::Notify user of completion with a message box
 powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Installation Complete', 'Software Install', 'OK', [System.Windows.Forms.MessageBoxIcon]::Information);}"
+::pause the shell
 pause
