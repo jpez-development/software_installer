5e9f3cf8b444beee529c8a76f1ecf9c0a7ceaa06
Joshua Perry <45966243+jpez-development@users.noreply.github.com>
2022-04-27 18:43:34 +0100

    formatting change

--- install.py
+++ install.py
@@ -5,5 +5,6 @@ import os, sys
 
 #loop through lines in text file
 for line in open(f"{sys.argv[1]}software_list.txt", "r"):
+    
     #install software and print command output to cmd
     print(os.system(f"choco install -y {line}"))
